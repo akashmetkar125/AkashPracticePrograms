aac72536be14bbc4e9e79cc62e0384401b3fd134
Akash <akashrmetkar121@gmail.com>
2024-08-02 23:34:24 +0530

    added two new classess exercise1 and 2

---
+++ src/test/java/Assignment/Exercise3.java
@@ -0,0 +1,18 @@
+package Assignment;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 final int i=2;
+		    int j=4;
+		    j = i;
+		        System.out.println("i = "+ i);
+		        System.out.print("j = "+ j);
+				
+		       }
+
+	}
+
+
---
+++ src/test/java/Assignment/exercise2.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+public class exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		   boolean k=false;
+		   final boolean b=false;
+		
+		    System.out.println("k=" +k);
+		    System.out.println("b=" +b);
+		     }
+
+
+	}
+
